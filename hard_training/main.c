9fed9099c8298cc41ca40e36fa4d40f20c4c90c8
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-03 16:55:46 +0200

    entrainement macro prédéfinies

--- hard_training/main.c
+++ hard_training/main.c
@@ -8,5 +8,6 @@ int main()
     int bonus = 20;
     bonus = DOUBLER(bonus);
     printf("bonus = %d\n", bonus);
+    printf("Je suis à la ligne %d et j'ai été écris le %s\n", __LINE__, __DATE__);
     return 0;
 }
