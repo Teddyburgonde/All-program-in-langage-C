2d01c5e4062b82aa0eddbcbe4adf867222a5028d
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-03 16:32:16 +0200

    entrainement macro

--- hard_training/main.c
+++ hard_training/main.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAILLE_TABLEAU 15
-#define NUM_VERSION "V01R00"
-
-#define NB_HEURE_PAR_JOUR 24
-#define NB_MINUTE_PAR_HEURE 60
-#define NB_HEURE_PAR_JOUR (NB_HEURE_PAR_JOUR*NB_MINUTE_PAR_HEURE)
+#define DOUBLER(x) ((x)*2)
 
 int main()
 {
-    printf("version: %s\n", NUM_VERSION);
-    printf("3 jours = %d\n", NB_HEURE_PAR_JOUR *3);
-    printf("3 jours = %d\n"), (NB_HEURE_PAR_JOUR*NB_MINUTE_PAR_HEURE) *3);
-    printf("3 jours = %d\n", (24*60)*3);
-
-    int tableau[TAILLE_TABLEAU];
-
-    for int i=0; i<TAILLE_TABLEAU; i++;
-    {
-        mon_tableau[i] = i;
-        printf("tab[%d] = %d\n", i, mon_tableau[i]);
-    }
-
+    int bonus = 20;
+    bonus = DOUBLER(bonus);
+    printf("bonus = %d\n", bonus);
     return 0;
 }
