db3483eab3ab588b9cd74cea0343def3b905ef35
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-04 13:12:38 +0200

    modif

--- hard_training/main.c
+++ hard_training/main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DOUBLER(x) ((x)*2)
 
 int main()
 {
-    int bonus = 20;
-    bonus = DOUBLER(bonus);
-    printf("bonus = %d\n", bonus);
-    printf("Je suis à la ligne %d et j'ai été écris le %s\n", __LINE__, __DATE__);
+
     return 0;
 }
