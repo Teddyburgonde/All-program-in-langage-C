1c29c5f1ec5d6a1e78d7e61841598cfd2e1fd58a
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-04 08:19:26 +0200

    erreur dans la déclaration macro , stay 2 arguments but give one argument

--- tp_10/main.c
+++ tp_10/main.c
@@ -5,7 +5,9 @@
 #define VERSION_PROGRAMME "R2D2"
 #define AUTEUR_PROGRAMME "Teddy"
 #define AUTEUR_EMAIL "banane.com"
-
+//macro
+#define A_PROPOS(titre) ("TP10: le preprocesseur")
+#define DEBUG_LOG(niveau, message) (("CRITIC,ERROR ou DEBUG"),("le message %s\n", __FILE__)
 
 int main()
 {
@@ -13,13 +15,16 @@ int main()
 
     putchar('\n');
 
-    #define A_PROPOS(titre) ("TP10: le preprocesseur")
 
     #ifndef DEBUG
         printf("%s\n", A_PROPOS(titre));
         printf("La version du programme est %s\n", VERSION_PROGRAMME);
         printf("Il a était compiler %s à %s\n", __DATE__, __TIME__);
         printf("Il a était compiler par %s\n", AUTEUR_PROGRAMME);
+    #endif
+
+    #ifdef DEBUG_LOG
+        printf("%s\n", DEBUG_LOG((message, niveau));
     #endif
 
     return 0;
