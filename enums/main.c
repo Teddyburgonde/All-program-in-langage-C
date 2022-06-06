f6290854cbf811717ab4ef376fbd4628b9e827da
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-06 14:12:55 +0200

    fin du projet enums

--- enums/main.c
+++ enums/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "personnage.h"
 
 int main()
 {
-
+    enum CLASSE perso = CLASSE_PALADIN;
+    printf("Mon personnage est de classe %d\n", perso);
     return 0;
 }
