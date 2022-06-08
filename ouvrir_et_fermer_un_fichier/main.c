bad23d1a858432af6d28b3d30170d7a651248001
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-08 18:41:39 +0200

    ajout de flclose

--- ouvrir_et_fermer_un_fichier/main.c
+++ ouvrir_et_fermer_un_fichier/main.c
@@ -3,11 +3,19 @@
 
 int main()
 {
-    FILE* pt_fichier = fopen("data/readme.txt","r");
+    FILE* pt_fichier = fopen("readme.txt","r");
 
     if(pt_fichier == NULL)
     {
         printf("Erreur lors de l'ouverture du fichier\n");
+        return -1;
+    }
+
+    int resultat = fclose(pt_fichier);
+    if(resultat !=0)
+    {
+        printf("Erreur lors de la fermeture du fichier");
+        return -2;
     }
 
     return 0;
