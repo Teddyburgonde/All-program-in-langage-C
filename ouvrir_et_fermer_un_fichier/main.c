b54765151117d2ebd5f6cac74e32e883e2bdf391
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-08 18:06:52 +0200

    création fichier readme.txt dans data

---
+++ ouvrir_et_fermer_un_fichier/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE* pt_fichier = fopen("readme.txt","r");
+
+    if(pt_fichier == NULL)
+    {
+        printf("Erreur lors de l'ouverture du fichier\n");
+    }
+
+    return 0;
+}
