80e3adf8dd637d28ef4da9b01b4f711bf10b8cd0
Teddy Bandama <bandama.teddy@gmail.com>
2022-06-24 11:09:04 +0200

    j'ai codé la fonction qui permet d'afficher les nombres paires contenu dans un tableau

--- tableau_paire/main.c
+++ tableau_paire/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void nombrePaires(int* tab, int taille)
 {
-
-    int tab[4] = {12, 23, 34, 45};
-
-    for (int i = 0; i < 4; i++)
+    for(int i = 0; i < 4; i++)
     {
-        if(tab[i] % 2 == 0)
+        if (tab[i] % 2 == 0)
         {
-            printf("Le nombre %d est pair \n", tab[i]);
+            printf("Le nombre %d est pair\n", tab[i]);
         }
     }
-
     return 0;
 }
+
+int main( int argc, const char * argv[])
+{
+    int tab[4] = {12, 23, 34, 45};
+    nombrePaires(tab, 4);
+
+}
+
